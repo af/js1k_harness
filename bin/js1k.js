6b433198b995b96b4bb68105203e8876c753a803
Aaron Franks <aaron.franks@gmail.com>
2013-02-09 16:17:43 -0800

    Implement basic 'serve' and 'init' subcommands.

--- bin/js1k.js
+++ bin/js1k.js
@@ -3,7 +3,32 @@
 var cli = require('commander');
 var js1k = require('../lib');
 
+
 cli.version(require('../package').version);
+
+cli.command('init')
+   .description('Begin a new js1k entry')
+   .usage('<filename>')
+   .action(function(fileName) {
+       if (typeof fileName !== 'string') fileName = 'js1k_entry.js';
+       js1k.initProject(fileName);
+   });
+
+cli.command('serve')
+   .usage('<filename>')
+   .description('Start a web server for the shim html and minified js')
+   .action(function(fileName) {
+       if (typeof fileName !== 'string') fileName = 'js1k_entry.js';
+       js1k.serve(fileName);
+   });
+
+cli.command('build')
+   .description('Build and compress the current entry js file')
+   .action(js1k.build);
+
+cli.command('size')
+   .description('Output this size of the current entry js (in bytes)')
+   .action(js1k.size);
 
 cli.command('rules')
    .description('Open the js1k rules page')
---
+++ boilerplate.js
@@ -0,0 +1,7 @@
+// This is your js1k entry. This script will be minified and stripped of comments.
+// The following variables are available to this script:
+//     a: <canvas> 2d drawing context
+//     b: document.body
+//     c: <canvas> DOM element
+
+
